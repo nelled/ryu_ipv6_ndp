ca4078db44e51eeebe0d5c0b74460b0599fcf735
veitsupateit <dnelle@uni-potsdam.de>
2018-10-26 15:47:51 +0800

    cache NS probing seems to work

--- cache_manager.py
+++ cache_manager.py
@@ -49,9 +49,8 @@ class CacheManager(SimpleSwitch13):
 
             # TODO: Maybe probe counter? Should we abort after some attempts
             # Or resend every update interval until deletion?
-            if age > 1.2 * REACHABLE_TIME
-                # Get suffix from entry
-                self._send_ns(v.ip, CONTROLLER_NC_SOURCE)
+            if age > 1.2 * REACHABLE_TIME:
+                self._send_ns(v)
                 print('Probe sent')
                 v.set_probe()
             if age > 3 * REACHABLE_TIME:
@@ -76,23 +75,27 @@ class CacheManager(SimpleSwitch13):
     # TODO: refactor functions, we do not need to pass a src here if we always use one MAC
     # TODO: Look at actions and parser in _send_ra, need todo this as well
     # TODO: Put this in wiki, Thomas' code did not work because no ether_head and possibly no prefix info
-    def _create_ns(self, dst, src):
+    def _create_ns(self, cache_entry):
+        print('IP:' + cache_entry.ip)
+        print('MAC:' + cache_entry.mac)
         # Solicitation
-        ether_head = scapy.Ether(src="70:01:02:03:04:05", dst="33:33:00:00:00:01")
+        ether_head = scapy.Ether(dst=cache_entry.mac)
         ipv6_head = scapy.IPv6()
-        ipv6_head.dst = self._make_sn_mc(dst)
-
-        icmpv6_ns = scapy.ICMPv6ND_NS(tgt=dst)
+        # Solicited node multicast
+        ipv6_head.dst = self._make_sn_mc(cache_entry.ip)
+        # Global address?
+        icmpv6_ns = scapy.ICMPv6ND_NS(tgt=cache_entry.ip)
         icmpv6_opt_pref = scapy.ICMPv6NDOptPrefixInfo()
-        llSrcAdd = scapy.ICMPv6NDOptSrcLLAddr(lladdr=src)
+        # Is this the address the answer will be sent to?
+        llSrcAdd = scapy.ICMPv6NDOptSrcLLAddr(lladdr=CONTROLLER_NC_SOURCE)
 
         sol = (ether_head / ipv6_head / icmpv6_ns / icmpv6_opt_pref/ llSrcAdd)
-        #print(sol.show())
+        print(sol.show())
 
         return sol
 
-    def _send_ns(self, dst, src):
-        pkt = self._create_ns(dst, src)
+    def _send_ns(self, cache_entry):
+        pkt = self._create_ns(cache_entry)
         self._send_all(pkt)
 
     def _send_all(self, pkt):
